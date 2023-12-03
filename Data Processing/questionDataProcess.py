976da1d46af4d82c990635c12dbd671446edbb52
Wzs010429 <1119127845@qq.com>
2023-12-04 11:18:25 +1300

    Process question data

---
+++ Data Processing/questionDataProcess.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def sort_and_save_csv(file_path):
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(file_path)
+
+    # Sorting the DataFrame first by 'Class' and then by 'Keyword'
+    sorted_df = df.sort_values(by=['Class', 'Keyword'])
+
+    # Write the sorted DataFrame back to the original file
+    sorted_df.to_csv(file_path, index=False)
+
+
+sort_and_save_csv('Questions - Sheet1.csv')
