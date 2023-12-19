6e9e3299db99529db011b4b3497c48b1e7c29943
Wzs010429 <1119127845@qq.com>
2023-12-19 13:52:09 +1300

    Generate answer

--- Data Processing/AnswerGeneration.py
+++ Data Processing/AnswerGeneration.py
@@ -51,23 +51,80 @@ def answer_generation(data, question):
   return completion.choices[0].message["content"]
 
 
-# 打开新的CSV文件进行写入
-with open('QA_new.csv', mode='w', newline='', encoding='utf-8') as output_csvfile:
-    writer = csv.writer(output_csvfile)
+# # 打开新的CSV文件进行写入
+# with open('QA_new.csv', mode='w', newline='', encoding='utf-8') as output_csvfile:
+#     writer = csv.writer(output_csvfile)
+#
+#     # 写入新CSV文件的表头
+#     writer.writerow(['Class', 'Issue', 'Question', 'Answer'])
+#     # 遍历每个data元素
+#     for d in data:
+#         # 读取原始CSV文件
+#         with open('Question.csv', newline='', encoding='utf-8') as input_csvfile:
+#             questionNo = 0
+#             reader = csv.DictReader(input_csvfile)
+#
+#             # 对于每个问题，生成答案并写入新CSV文件
+#             for row in reader:
+#                 question = row['Question']
+#                 answer = answer_generation(d, question)
+#                 writer.writerow([row['Class'], row['Issue'], question, answer])
+#                 questionNo += 1
+#                 print(f"Question {questionNo}: Answer generated.")
 
-    # 写入新CSV文件的表头
-    writer.writerow(['Class', 'Issue', 'Question', 'Answer'])
-    # 遍历每个data元素
-    for d in data:
-        # 读取原始CSV文件
-        with open('Question.csv', newline='', encoding='utf-8') as input_csvfile:
-            questionNo = 0
-            reader = csv.DictReader(input_csvfile)
 
-            # 对于每个问题，生成答案并写入新CSV文件
-            for row in reader:
-                question = row['Question']
-                answer = answer_generation(d, question)
-                writer.writerow([row['Class'], row['Issue'], question, answer])
-                questionNo += 1
-                print(f"Question {questionNo}: Answer generated.")
+question_list = ["Can you provide a detailed sale history of this property, including dates and sale prices?",
+                    "How many times has this property changed hands in the past 20 years?",
+                    "Were there any significant fluctuations in the sale price of this property during its history?",
+                    "What can you tell me about the duration of ownership between each sale of this property?",
+                    "Does the sale history of this property show any distressed sales, such as foreclosures or short sales?",
+                    "Can you provide a detailed sale history of the property, including dates and sale prices of past transactions?",
+                    "Has the property been frequently bought and sold in the past, and if so, can you explain any reasons for this high turnover?",
+                    "Are there any notable trends or irregularities in the property's sale history that might impact its current valuation?",
+                    "Does the sale history of this house reveal any foreclosures or short sales that could affect my potential purchase?"]
+
+data = [{
+          "property_id": "NZ01103666/4D",
+          "sale_id": "20159316",
+          "sale_date": "2012-04-04",
+          "sale_price_gross": "258000",
+          "agree_date": "2012-04-04",
+          "sale_type": "Whole - Freehold market level",
+          "sale_price_chattels": "null"
+        }, {
+          "property_id": "NZ01103666/4D",
+          "sale_id": "20205959",
+          "sale_date": "2014-02-05",
+          "sale_price_gross": "335000",
+          "agree_date": "2014-02-05",
+          "sale_type": "Whole - Freehold market level",
+          "sale_price_chattels": "null"
+        }, {
+          "property_id": "NZ01103666/4D",
+          "sale_id": "20460197",
+          "sale_date": "2002-08-12",
+          "sale_price_gross": "174000",
+          "agree_date": "2002-08-12",
+          "sale_type": "Whole - Freehold market level",
+          "sale_price_chattels": "5000"
+        }, {
+          "property_id": "NZ01103666/4D",
+          "sale_id": "20720738",
+          "sale_date": "2006-03-21",
+          "sale_price_gross": "240000",
+          "agree_date": "2006-03-21",
+          "sale_type": "Whole - Freehold market level",
+          "sale_price_chattels": "8000"
+        }, {
+          "property_id": "NZ01103666/4D",
+          "sale_id": "24553089",
+          "sale_date": "2017-10-02",
+          "sale_price_gross": "250000",
+          "agree_date": "2017-10-02",
+          "sale_type": "Whole - Freehold market level",
+          "sale_price_chattels": "8000"
+        }]
+
+
+for question in question_list:
+    print(answer_generation(data, question))
